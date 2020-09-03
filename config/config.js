414296055e8401c12e704a524e28cd3c79f33897
ullagulla <ulrikaa91@hotmail.com>
2020-09-03 16:02:21 +0200

    fixing send email to customer on booking and cancel booking

--- config/config.js
+++ config/config.js
@@ -1,7 +1,11 @@
+const { getMaxListeners } = require("..");
+
 require("dotenv").config()
 
 const config = {
     databaseURL: process.env.DATABASE,
+    email: "purplenurples007@gmail.com",
+    password: "Nurples007",
 }
 
 module.exports = config;
